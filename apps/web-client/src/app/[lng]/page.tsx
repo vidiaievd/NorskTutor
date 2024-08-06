e3765ebb5a807937385afaf1302fd19bda7bfd3f
Dmytro Vidiaiev <vidiaievdmytro@gmail.com>
2024-08-06 20:42:27 +0200

    add theme

--- apps/web-client/src/app/[lng]/page.tsx
+++ apps/web-client/src/app/[lng]/page.tsx
@@ -1,8 +1,14 @@
-export default function Index() {
+import { Button } from '@nx-shadcn-ui';
+import { ModeToggle } from '../../components/switch-theme-button';
 
+export default function Page() {
   return (
-    <div>
-      <p>Hello world</p>
+    <>
+    <div className="bg-indigo-500 p-2 font-mono">
+      <Button>Click me!</Button>
     </div>
+      <Button>Click me!</Button>
+      <ModeToggle />
+      </>
   );
 }
